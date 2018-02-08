88642a629e52a30a3c4c8dcfa8f96d80e2b68551
eldercharles <eldercharlespsg@gmail.com>
2018-02-08 14:26:34 -0300

    Sistema de Gerar Pdf

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+
+                // function doc_print(){
+                //     window.print();
+                // }
+                // window.onload = function(){
+                //     // window.print(); (opicional)
+                //     doc_print();
+                // }
+            
