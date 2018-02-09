a8f2b294e7a5fdc3c5da0487d66ac9570abdce06
Elder Charles da Silva <esilva@PSG.LOCAL>
2018-02-09 17:02:16 -0300

    Atualizar Pdf

--- Js/script.js
+++
@@ -1,9 +0,0 @@
-
-                // function doc_print(){
-                //     window.print();
-                // }
-                // window.onload = function(){
-                //     // window.print(); (opicional)
-                //     doc_print();
-                // }
-            
